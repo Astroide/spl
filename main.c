b9f895bd078ef445dc7df11514ddb270f3b67fb4
Olie Auger <olie.auger@gmail.com>
2021-08-01 10:29:02 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    // int c;
+    // FILE *file;
+    // file = fopen("main.c", "r");
+    // if (file) {
+        // while ((c = getc(file)) != EOF)
+            // putchar(c);
+        // fclose(file);
+    // }
+    printf("SPL - Some Programming Language");
+    return 0;
+}
